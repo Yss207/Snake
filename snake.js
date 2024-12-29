3300dba33f9542f09cf5e5161c17559969039556
Yss207 <suryawanshiyash04@gmail.com>
2024-12-29 14:11:20 +0530

    fourth

--- snake.js
+++ snake.js
@@ -5,6 +5,10 @@ var cols = 20;
 var board;
 var context;
 
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = rows * blockSize;
@@ -17,4 +21,7 @@ window.onload = function () {
 function update() {
   context.fillStyle = "black";
   context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = "lime";
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
 }
